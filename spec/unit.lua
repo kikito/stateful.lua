fa55eedd9b52d38879fb7750229edd62a033489a
Enrique García Cota <kikito@gmail.com>
2011-09-25 17:33:08 +0200

    simple acceptance test passes

--- spec/unit.lua
+++ spec/unit.lua
@@ -2,3 +2,42 @@ require 'spec/lib/middleclass'
 
 Stateful = require 'stateful'
 
+describe("Unit tests", function()
+
+  describe("including the mixin", function()
+    test("The class gets a new class attribute called 'states'", function()
+      local C = class('C')
+      C:include(Stateful)
+      assert_type(C.states, "table")
+    end)
+  end)
+
+  describe("addState", function()
+    describe("When given a valid state name", function()
+      test("class.states contains an entry with that name", function()
+        local C = class('C'):include(Stateful)
+        C:addState("State")
+        assert_type(C.states.State, "table")
+      end)
+    end)
+  end)
+
+  describe("gotoState", function()
+    describe("when given a valid state name", function()
+      test("the class instances use that state methods instead of the default ones", function()
+        local Enemy = class('Enemy'):include(Stateful)
+        function Enemy:foo() return 'foo' end
+        local SayBar = Enemy:addState('SayBar')
+        function SayBar:foo() return 'bar' end
+
+        local e = Enemy:new()
+        assert_equal(e:foo(), 'foo')
+        e:gotoState('SayBar')
+        assert_equal(e:foo(), 'bar')
+
+      end)
+    end)
+  end)
+
+
+end)
--- stateful.lua
+++ stateful.lua
@@ -1,11 +1,32 @@
-local Stateful = {}
+local Stateful = { static = {} }
 
-function Stateful:addState()
-  return {}
+local function _modifyInstanceDict(klass)
+  local prevIndex = klass.__instanceDict.__index
+
+  klass.__instanceDict.__index = function(instance, name)
+    local currentState = rawget(instance, '__currentState')
+    if currentState and currentState[name] then return currentState[name] end
+
+    if type(prevIndex) == 'function' then return prevIndex(instance, name) end
+    return prevIndex[name]
+  end
+
 end
 
-function Stateful:gotoState()
+function Stateful:included(klass)
+  klass.static.states = {}
+  _modifyInstanceDict(klass)
+end
 
+function Stateful.static:addState(stateName)
+  self.static.states[stateName] = {}
+  return self.static.states[stateName]
+end
+
+function Stateful:gotoState(stateName)
+  local state = self.class.states[stateName]
+
+  self.__currentState = state
 end
 
 return Stateful
