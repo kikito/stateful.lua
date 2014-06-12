3bcc3f4960f97028db353c2285c62603ba439961
kikito <kikito@gmail.com>
2014-06-13 00:30:48 +0200

    release 1.0.2

--- stateful.lua
+++ stateful.lua
@@ -1,5 +1,5 @@
--- stateful.lua - v1.0.1 (2011-10)
--- requires middleclass 2.0
+-- stateful.lua - v1.0.2 (2011-10)
+-- requires middleclass >2.0
 
 -- Copyright (c) 2011 Enrique García Cota
 -- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
